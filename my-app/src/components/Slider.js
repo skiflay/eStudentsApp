f12175f4dcf924f208bc048af2cce992cc646035
skiflay <skiflay@miu.edu>
2022-01-11 15:03:59 -0600

    UI-design

---
+++ my-app/src/components/Slider.js
@@ -0,0 +1,16 @@
+
+const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+`
+
+const Slider = ()=>{
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Slider
