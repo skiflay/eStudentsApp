e416644b78923bf3f5f7a323f369782f14cac2c3
skiflay <skiflay@miu.edu>
2022-01-11 20:10:10 -0600

    updated slider comp

--- my-app/src/Pages/Home.js
+++ my-app/src/Pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Slider from '../components/Slider'
 function Home() {
     return (
         <div>
             <Navbar />
+            <Slider />
         </div>
     )
 }
