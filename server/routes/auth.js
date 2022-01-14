8ea00379cb04ca1c160768ff2e021006e760a718
skiflay <skiflay@miu.edu>
2022-01-13 23:01:15 -0600

    JWT added

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const cryptoJS = require('crypto-js')
+const jwt = require('jsonwebtoken')
 const User = require('../models/User')
 
 //Register
@@ -7,7 +9,7 @@ router.post("/register", async (req, res)=>{
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password,
+        password: cryptoJS.AES.encrypt(req.body.password, process.env.PWD_SECRET).toString()
     });
     try{
         const savedUser = await newUser.save()
@@ -18,6 +20,31 @@ router.post("/register", async (req, res)=>{
     }
 })
 
+// Login
+router.post('/login', async (req, res)=>{
+    try{
+        const user = await User.findOne({username: req.body.username})
+        !user && res.status(401).json('Wrong Credentials!');
+
+        // Token
+        const accessToken = jwt.sign(
+            {
+            id: user._id,
+            isAdmin: user.isAdmin
+        }, 
+        process.env.JWT_SECRET, {expiresIn: "3d"}
+        )
+        //Decrypt PWD
+        const hashedPwd = cryptoJS.AES.decrypt(user.password, process.env.PWD_SECRET)
+        const password = hashedPwd.toString(cryptoJS.enc.Utf8)
+        console.log(password)
+
+        password !== req.body.password && res.status(401).json('Wrong Credentials!')
+        res.status(200).json({...user._doc, accessToken})
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
 
 
 module.exports = router
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,7 +1,25 @@
 const express = require('express')
+const cryptoJS = require('crypto-js')
+const User = require('../models/User')
 const router = express.Router()
+const {verifyToken, verifyTokenANdAuthorization} = require('./verifyToken')
 
-
+//Update
+router.put('/:id', verifyTokenANdAuthorization, async (req, res)=>{
+    // if(req.user.id === req.params.id || req.user.isAdmin)
+    if(req.body.password){
+        req.body.password = cryptoJS.AES.encrypt(req.body.password, process.env.PWD_SECRET).toString()   
+    }
+    try{
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            $set: req.body
+        }, {new: true})
+        console.log(req.body, req.params.id)
+        res.status(200).json(updatedUser)
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
 
 
 module.exports = router
